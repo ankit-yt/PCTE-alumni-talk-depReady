7e0bba084a4cc73049216480c8cedf559d047236
ankit-yt <ankitpk3303@gmail.com>
2025-10-12 11:00:29 +0530

    commit 4

--- reactFrontend/src/utils/axiosInstance.js
+++ reactFrontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://pcte-alumni-talk-depready.onrender.com/",
+  baseURL: "http://localhost:3000/",
   withCredentials: true,
 });
 
